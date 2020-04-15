1cb00f2689eae95787e83e2446a7af5191f55b49
Molonlabee <janayahernandez@yahoo.com>
2020-04-15 19:59:06 -0400

    third

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -3,7 +3,7 @@ import spinner from './octocat-spinner-smil.min.svg';
 
 const Spinner = () => 
         <Fragment>
-            <img src={spinner} alt='Loading...' style={{width: '400px', margin: 'auto', display: 'block'}} />
+            <img src={spinner} alt='Loading...' style={{width: '200px', margin: 'auto', display: 'block'}} />
         </Fragment>
   
 
